22bb6d3e8887e15e0c5861322035bde6eb77ad37
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2019-05-08 17:23:10 -0400

    new setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name='emailanalysis',
-      version='0.1',
       description='Analyze email d',
       url='https://github.com/dmil/EmailAnalysis',
       author='Dhrumil Mehta',
