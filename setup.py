1d1744446df2de19ab3931b7b4b5e2c083b9cf8c
Dhrumil Mehta <dhrumil.mehta@gmail.com>
2019-05-08 16:15:45 -0400

    try to set up as python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='emailanalysis',
+      version='0.1',
+      description='Analyze email d',
+      url='https://github.com/dmil/EmailAnalysis',
+      author='Dhrumil Mehta',
+      author_email='dhrumil.mehta@gmail.com',
+      packages=['emailanalysis'],
+      )
